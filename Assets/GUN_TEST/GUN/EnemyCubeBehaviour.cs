a46fb21cb1667105dcf9e4e45a74cc643ef8256e
SchizoCat3D <schizocat3d@gmail.com>
2017-11-29 13:41:08 +0100

    Más Cosas Random

---
+++ Assets/GUN_TEST/GUN/EnemyCubeBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCubeBehaviour : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/GUN_TEST/GUN/InputManagerRightHand.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManagerRightHand : MonoBehaviour
+{
+    public bool trigger;
+    public bool grap;
+    public bool buttonA;
+    public bool buttonB;
+    // Use this for initialization
+    void Start ()
+    {
+        trigger = false;
+        grap = false;
+        buttonA = false;
+        buttonB = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            Trigger();
+        }
+        else trigger = false;
+        if (OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
+        {
+            grap = true;
+        }
+        else grap = false;
+        if (OVRInput.Get(OVRInput.Button.One))
+        {
+            buttonA = true;
+        }
+        else buttonA = false;
+        if (OVRInput.Get(OVRInput.Button.Two))
+        {
+            buttonB = true;
+        }
+        else buttonB = false;
+    }
+
+    void Trigger()
+    {
+        trigger = true;
+    }
+}
